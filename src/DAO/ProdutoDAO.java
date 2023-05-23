0e83b87b3eddcf4ca5e82d9037119fa93229c061
Diogo Sarti <diogosarti13@gmail.com>
2023-05-23 13:43:39 -0300

    CRUD added

--- src/DAO/ProdutoDAO.java
+++ src/DAO/ProdutoDAO.java
@@ -11,16 +11,17 @@ import javax.swing.JOptionPane;
 import java.lang.NumberFormatException;
 
 public class ProdutoDAO {
+
     Connection conn;
     PreparedStatement pstm;
     ResultSet rs;
-    
+
     ArrayList<ProdutoDTO> produtoLista = new ArrayList<>();
-    
-    public void cadastrarProduto(ProdutoDTO produto){
+
+    public void cadastrarProduto(ProdutoDTO produto) {
         String sql = "insert into produto(nome_produto, preco_produto, qtdeEstoque_produto) values(?, ?, ?)";
         conn = new ConexaoDAO().conectaDB();
-        
+
         try {
             pstm = conn.prepareStatement(sql);
             pstm.setString(1, produto.getNomeProduto());
@@ -30,28 +31,28 @@ public class ProdutoDAO {
             pstm.execute();
             pstm.close();
             JOptionPane.showMessageDialog(null, "Produto cadastrado com sucesso");
-        } catch(NumberFormatException e){
+        } catch (NumberFormatException e) {
             JOptionPane.showMessageDialog(null, e);
         } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, e);
         }
     }
-    
-    public ArrayList<ProdutoDTO> pesquisarProduto(){
+
+    public ArrayList<ProdutoDTO> pesquisarProduto() {
         String sql = "select * from produto";
         conn = new ConexaoDAO().conectaDB();
         try {
             pstm = conn.prepareStatement(sql);
             rs = pstm.executeQuery();
-            
-            while (rs.next()) {                
+
+            while (rs.next()) {
                 ProdutoDTO objProdutoDTO = new ProdutoDTO();
                 objProdutoDTO.setIdPruduto(rs.getInt("id_produto"));
                 objProdutoDTO.setNomeProduto(rs.getString("nome_produto"));
                 objProdutoDTO.setPrecoProduto(rs.getDouble("preco_produto"));
                 objProdutoDTO.setQtdeEstoqueProduto(rs.getInt("qtdeEstoque_produto"));
                 //objProdutoDTO.setIdFornecedorProduto(rs.getInt("idFornecedor_produto"));
-                
+
                 produtoLista.add(objProdutoDTO);
             }
             pstm.close();
@@ -59,7 +60,90 @@ public class ProdutoDAO {
             JOptionPane.showMessageDialog(null, e.getMessage());
             return null;
         }
-        
+
         return produtoLista;
     }
+
+    public ProdutoDTO obterProdutoPorId(int idProduto) {
+        String sql = "SELECT * FROM produto WHERE id_produto = ?";
+        conn = new ConexaoDAO().conectaDB();
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, idProduto);
+            rs = pstm.executeQuery();
+
+            if (rs.next()) {
+                ProdutoDTO objProdutoDTO = new ProdutoDTO();
+                objProdutoDTO.setIdPruduto(rs.getInt("id_produto"));
+                objProdutoDTO.setNomeProduto(rs.getString("nome_produto"));
+                objProdutoDTO.setPrecoProduto(rs.getDouble("preco_produto"));
+                objProdutoDTO.setQtdeEstoqueProduto(rs.getInt("qtdeEstoque_produto"));
+                // objProdutoDTO.setIdFornecedorProduto(rs.getInt("idFornecedor_produto"));
+
+                return objProdutoDTO;
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (pstm != null) {
+                    pstm.close();
+                }
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, ex.getMessage());
+            }
+        }
+
+        return null;
+    }
+
+    public void removerProduto(int idProduto) throws SQLException {
+        String sql = "DELETE FROM produto WHERE id_produto = ?";
+        conn = new ConexaoDAO().conectaDB();
+
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, idProduto);
+
+            int rowsAffected = pstm.executeUpdate();
+            pstm.close();
+
+            if (rowsAffected > 0) {
+                JOptionPane.showMessageDialog(null, "Produto removido com sucesso");
+            } else {
+                JOptionPane.showMessageDialog(null, "Nenhum produto foi removido");
+            }
+        } catch (SQLException e) {
+            throw new SQLException("Erro ao remover o produto: " + e.getMessage());
+        }
+    }
+    
+    public void atualizarProduto(ProdutoDTO produto) throws SQLException {
+    String sql = "UPDATE produto SET nome_produto = ?, preco_produto = ?, qtdeEstoque_produto = ? WHERE id_produto = ?";
+    conn = new ConexaoDAO().conectaDB();
+
+    try {
+        pstm = conn.prepareStatement(sql);
+        pstm.setString(1, produto.getNomeProduto());
+        pstm.setDouble(2, produto.getPrecoProduto());
+        pstm.setInt(3, produto.getQtdeEstoqueProduto());
+        pstm.setInt(4, produto.getIdPruduto());
+
+        int rowsAffected = pstm.executeUpdate();
+        pstm.close();
+
+        if (rowsAffected > 0) {
+            JOptionPane.showMessageDialog(null, "Produto atualizado com sucesso");
+        } else {
+            JOptionPane.showMessageDialog(null, "Nenhum produto foi atualizado");
+        }
+    } catch (SQLException e) {
+        throw new SQLException("Erro ao atualizar o produto: " + e.getMessage());
+    }
+}
+
 }
--- src/VIEW/Paineis/painelProduto.java
+++ src/VIEW/Paineis/painelProduto.java
@@ -2,6 +2,7 @@ package VIEW.Paineis;
 
 import DAO.ProdutoDAO;
 import DTO.ProdutoDTO;
+import VIEW.frmAlterarProduto;
 import VIEW.frmCadastrarPorduto;
 import javax.swing.border.EmptyBorder;
 import java.sql.SQLException;
@@ -12,11 +13,10 @@ import util.buttoms.ActionButton;
 import util.buttoms.PanelAction;
 import util.buttoms.TableActionCellRender;
 
-
 public class painelProduto extends javax.swing.JPanel {
 
     frmCadastrarPorduto frmCadastro = null;
-    
+
     public painelProduto() {
         initComponents();
         listarValoresProduto();
@@ -26,26 +26,27 @@ public class painelProduto extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnAddProduct = new util.buttoms.MenuButton();
+        btnRefreshProduct = new util.buttoms.MenuButton();
         btnAddProduct1 = new util.buttoms.MenuButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         tabelaProdutos = new javax.swing.JTable();
-        btnAddProduct2 = new util.buttoms.MenuButton();
+        btnRemoveProduct = new util.buttoms.MenuButton();
+        btnEditProduct = new util.buttoms.MenuButton();
 
         setBackground(new java.awt.Color(255, 255, 255));
         setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        btnAddProduct.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/refresh.png"))); // NOI18N
-        btnAddProduct.setColorHover(new java.awt.Color(0, 153, 255));
-        btnAddProduct.setColorNormal(new java.awt.Color(0, 102, 204));
-        btnAddProduct.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        btnAddProduct.setIconTextGap(1);
-        btnAddProduct.addActionListener(new java.awt.event.ActionListener() {
+        btnRefreshProduct.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/refresh.png"))); // NOI18N
+        btnRefreshProduct.setColorHover(new java.awt.Color(0, 153, 255));
+        btnRefreshProduct.setColorNormal(new java.awt.Color(0, 102, 204));
+        btnRefreshProduct.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnRefreshProduct.setIconTextGap(1);
+        btnRefreshProduct.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAddProductActionPerformed(evt);
+                btnRefreshProductActionPerformed(evt);
             }
         });
-        add(btnAddProduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 10, 70, 60));
+        add(btnRefreshProduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 10, 70, 60));
 
         btnAddProduct1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/addIcon1.png"))); // NOI18N
         btnAddProduct1.setText("Cadastrar Produto");
@@ -58,7 +59,7 @@ public class painelProduto extends javax.swing.JPanel {
                 btnAddProduct1ActionPerformed(evt);
             }
         });
-        add(btnAddProduct1, new org.netbeans.lib.awtextra.AbsoluteConstraints(670, 10, 250, 60));
+        add(btnAddProduct1, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 10, 250, 60));
 
         tabelaProdutos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -90,53 +91,110 @@ public class painelProduto extends javax.swing.JPanel {
 
         add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(39, 87, 970, 501));
 
-        btnAddProduct2.setBackground(new java.awt.Color(204, 0, 0));
-        btnAddProduct2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/delete_1.png"))); // NOI18N
-        btnAddProduct2.setColorHover(new java.awt.Color(255, 0, 0));
-        btnAddProduct2.setColorNormal(new java.awt.Color(204, 0, 0));
-        btnAddProduct2.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
-        btnAddProduct2.setIconTextGap(1);
-        btnAddProduct2.addActionListener(new java.awt.event.ActionListener() {
+        btnRemoveProduct.setBackground(new java.awt.Color(204, 0, 0));
+        btnRemoveProduct.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/delete_1.png"))); // NOI18N
+        btnRemoveProduct.setColorHover(new java.awt.Color(255, 0, 0));
+        btnRemoveProduct.setColorNormal(new java.awt.Color(204, 0, 0));
+        btnRemoveProduct.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnRemoveProduct.setIconTextGap(1);
+        btnRemoveProduct.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnAddProduct2ActionPerformed(evt);
+                btnRemoveProductActionPerformed(evt);
             }
         });
-        add(btnAddProduct2, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 10, 70, 60));
+        add(btnRemoveProduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(940, 10, 70, 60));
+
+        btnEditProduct.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icon/editIcon.png"))); // NOI18N
+        btnEditProduct.setColorHover(new java.awt.Color(204, 204, 0));
+        btnEditProduct.setColorNormal(new java.awt.Color(255, 204, 0));
+        btnEditProduct.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnEditProduct.setIconTextGap(1);
+        btnEditProduct.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEditProductActionPerformed(evt);
+            }
+        });
+        add(btnEditProduct, new org.netbeans.lib.awtextra.AbsoluteConstraints(860, 10, 70, 60));
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnAddProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddProductActionPerformed
+    private void btnRefreshProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRefreshProductActionPerformed
         listarValoresProduto();
-    }//GEN-LAST:event_btnAddProductActionPerformed
+    }//GEN-LAST:event_btnRefreshProductActionPerformed
 
     private void btnAddProduct1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddProduct1ActionPerformed
-        if(frmCadastro == null){
+        if (frmCadastro == null) {
             frmCadastro = new frmCadastrarPorduto();
             frmCadastro.setLocationRelativeTo(null);
-            
-        }frmCadastro.setVisible(true);
+
+        }
+        frmCadastro.setVisible(true);
+
+
     }//GEN-LAST:event_btnAddProduct1ActionPerformed
 
-    private void btnAddProduct2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnAddProduct2ActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_btnAddProduct2ActionPerformed
+    private void btnRemoveProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnRemoveProductActionPerformed
+        // Remover o item selecionado na tabela
+        int selectedRow = tabelaProdutos.getSelectedRow();
+        if (selectedRow != -1) {
+            int confirm = JOptionPane.showConfirmDialog(null, "Deseja remover o produto selecionado?", "Remover Produto", JOptionPane.YES_NO_OPTION);
+            if (confirm == JOptionPane.YES_OPTION) {
+                int idProduto = (int) tabelaProdutos.getValueAt(selectedRow, 0);
+                ProdutoDAO produto = new ProdutoDAO();
+                try {
+                    produto.removerProduto(idProduto);
+                    listarValoresProduto();
+                    JOptionPane.showMessageDialog(null, "Produto removido com sucesso.");
+                } catch (SQLException ex) {
+                    JOptionPane.showMessageDialog(null, "Erro ao remover o produto: " + ex.getMessage());
+                }
+            }
+        } else {
+            JOptionPane.showMessageDialog(null, "Selecione um produto para remover.");
+        }
+    }//GEN-LAST:event_btnRemoveProductActionPerformed
+
+    private void btnEditProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEditProductActionPerformed
+        int row = tabelaProdutos.getSelectedRow();
+
+        if (row >= 0) {
+            int idProduto = (int) tabelaProdutos.getValueAt(row, 0);
+
+            // Criar instância do formulário de alteração
+            frmAlterarProduto frmAlteracao = new frmAlterarProduto(idProduto);
+            frmAlteracao.setLocationRelativeTo(null);
+
+            // Exibir o formulário de alteração
+            frmAlteracao.setVisible(true);
+
+            // Atualizar a tabela após o formulário de alteração ser fechado
+            frmAlteracao.addWindowListener(new java.awt.event.WindowAdapter() {
+                @Override
+                public void windowClosed(java.awt.event.WindowEvent windowEvent) {
+                    listarValoresProduto();
+                }
+            });
+        }
+
+    }//GEN-LAST:event_btnEditProductActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private util.buttoms.MenuButton btnAddProduct;
     private util.buttoms.MenuButton btnAddProduct1;
-    private util.buttoms.MenuButton btnAddProduct2;
+    private util.buttoms.MenuButton btnEditProduct;
+    private util.buttoms.MenuButton btnRefreshProduct;
+    private util.buttoms.MenuButton btnRemoveProduct;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tabelaProdutos;
     // End of variables declaration//GEN-END:variables
 
-    private void listarValoresProduto(){
+    private void listarValoresProduto() {
         try {
             ProdutoDAO objProduto = new ProdutoDAO();
             DefaultTableModel model = (DefaultTableModel) tabelaProdutos.getModel();
             model.setNumRows(0);
-            
+
             ArrayList<ProdutoDTO> lista = objProduto.pesquisarProduto();
-            
+
             for (int i = 0; i < lista.size(); i++) {
                 model.addRow(new Object[]{
                     lista.get(i).getIdPruduto(),
@@ -145,7 +203,7 @@ public class painelProduto extends javax.swing.JPanel {
                     lista.get(i).getQtdeEstoqueProduto()
                 });
             }
-            
+
         } catch (Exception e) {
             JOptionPane.showInputDialog(null, e.getMessage());
         }
